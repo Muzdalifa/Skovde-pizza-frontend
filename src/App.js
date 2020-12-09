ca8339f0aa76d1e71815c31be4a8f5141f65c5cb
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-12-09 03:25:01 +0100

    create and render order (image testing)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import data from './data';
+import Order from './Order';
 
-function App() {
+const App = () => {
+  //const [orders,setOrders] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    <section className="App">
+      {data.map((order)=>{
+        return <Order key={order.orderId} {...order} />
+      })}
+    </section>
+  )
+  }
 export default App;
---
+++ src/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Order = (props)=>{
+  return (
+  <div>
+    < img src={props.pizza}/>
+  </div>
+  )
+};
+
+export default Order;
