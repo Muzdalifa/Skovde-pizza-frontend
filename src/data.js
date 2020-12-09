ed8d94ae3651950ae63d0f2efdb6c63b5b57e6e4
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-12-09 03:20:42 +0100

    add data file (array of objects)

---
+++ src/data.js
@@ -0,0 +1,66 @@
+export default[
+  {
+    customer_id:1,
+    customer_name:'Wahida Hamza',
+    adress:'Kigamboni',
+    orderId:1,
+    pizza: './asset/bacon_tomato.png',
+    Time: '12:30'
+  },
+  {
+    customer_id:2,
+    customer_name:'Salma Azani',
+    adress:'Chukwani',
+    orderId:2,
+    pizza: './asset/salami_bacon_pizza.jpg',
+    Time: '12:30'
+  },
+   {
+    customer_id:3,
+    customer_name:'Anna Johan',
+    adress:'Chukwani',
+    orderId:3,
+    pizza: './asset/salami_bacon_paper.jpg',
+    Time: '12:30'
+  },
+   {
+    customer_id:4,
+    customer_name:'Stephani Luis',
+    adress:'Chukwani',
+    orderId:4,
+    pizza: './asset/pizza-1-1.jpg',
+    Time: '12:00'
+  },
+   {
+    customer_id:5,
+    customer_name:'Annita Andas',
+    adress:'Chukwani',
+    orderId:5,
+    pizza: './asset/salami_bacon_pizza.jpg',
+    Time: '17:30'
+  },
+   {
+    customer_id:6,
+    customer_name:'Jannet Jakson',
+    adress:'Chukwani',
+    orderId:6,
+    pizza: './asset/shrimp.jpg',
+    Time: '14:40'
+  },
+   {
+    customer_id:7,
+    customer_name:'Patrik Helberg',
+    adress:'Chukwani',
+    orderId:7,
+    pizza: './asset/pizza_2in_1.jpg',
+    Time: '16:00'
+  },
+   {
+    customer_id:8,
+    customer_name:'Annars Tid',
+    adress:'Chukwani',
+    orderId:8,
+    pizza: './asset/pepperoni-i-tomatyi.jpg',
+    Time: '18:30'
+  },
+];
