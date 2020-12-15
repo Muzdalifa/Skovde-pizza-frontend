410b3cc4355e825899ebac59fc297dff8b7a2915
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-12-15 10:14:45 +0100

    add conditional display to details

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import './Details.css'
 
-const Details = ()=>{
-  return <div className='rightNav'>
-  </div>
-}
-export default Details
+const Details = (props)=>{
+
+  return (
+    <div className='details'>
+      {
+        props.selectedItem.id !== ''
+        ? <div>
+            <img src={props.selectedItem.image} />
+            <label>Pizza name: {props.selectedItem.name}</label>
+            <label>Cost: {props.selectedItem.cost}</label>
+            <button onClick>Add</button>
+          </div>
+        : null
+      } 
+    </div>
+  )
+  
+};
+
+export default Details;
