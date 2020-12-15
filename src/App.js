387105b1821c37c221c8bf00470f897dfc0570cc
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-12-15 10:18:39 +0100

    Add Details and OrderdItem components

--- src/App.js
+++ src/App.js
@@ -2,23 +2,32 @@ import React, {useState} from 'react';
 import data from './data';
 import Menu from './Menu';
 import Header from './Header'
-import Details from './Details'
-import Order from './Order';
+import OrderdItem from './OrderdItem'
+import Details from './Details';
 import './App.css'
 
 
 const App = () => {
+
+  const [selectedItem, setSelectedItem] = useState({id: '',image:'',name:'', Time:''});
+  const [orderdItems,setOrderdItems] = useState([]);
+
+  const addOrderdItem=(item)=>{
+    orderdItems.push(item)
+    setOrderdItems([...orderdItems])
+  }
+
+  const detailsProps = { selectedItem, addOrderdItem }
+  const menuProps = { data, selectedItem, setSelectedItem }
   return (
     <div className='container'>
       <header>
         <h3>Skövde Pizza</h3>
       </header>
       <div className='middleContent'>
-        <Menu data={data} />
-        <section className='orderList'>      
-          
-        </section>
-        <Details />
+        <Menu { ...menuProps}   />
+        <Details className='orderList' {...detailsProps}  />
+        <OrderdItem />
       </div>
       <footer>
         Skövde Pizza
--- src/Menu.js
+++ src/Menu.js
@@ -2,15 +2,13 @@ import React, {useState}from 'react';
 import './Menu.css';
 
 const Menu = (props)=>{
-
-  const [selectedItemId,setSelectedItemId] = useState(null);
-
+  
   return <div className='lefNav'>
     <h4>Menu</h4>
     {props.data.map((item)=><div 
-      className={`navLink ${item.id===selectedItemId ? 'selectedItem': '' }`} 
+      className={`navLink ${item.id===props.selectedItem.id ? 'selectedItem': '' }`} 
       key={item.id} 
-      onClick={()=>setSelectedItemId(item.id)}>{item.name}
+      onClick={()=>props.setSelectedItem(item)}>{item.name}
     </div>)}    
   </div>
 }
