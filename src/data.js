a0a425181c65dcb76133fbb91a4101ef92cadfb4
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-12-15 10:10:25 +0100

    Change data

--- src/data.js
+++ src/data.js
@@ -3,51 +3,54 @@ export default[
     id:1,
     image: './asset/bacon_tomato.png',
     name: 'Bacon tomato',
-    Time: '12:30'
+    cost: '120'
   },
   {
     id:2,
     image: './asset/salami.jpg',
     name: 'Salami',
-    Time: '12:30'
+    cost: '110'
   },
    {
     id:3,
     image: './asset/salami_bacon_paper.jpg',
     name: 'salami bacon paper',
-    Time: '12:30'
+    cost: '99'
   },
    {
     id:4,
     image: './asset/vegetarian.png',
     name: 'Vegetarian',
-    Time: '12:00'
+    cost: '70'
   },
    {
     id:5,
     image: './asset/OIP.jpg',
     name: 'OIP',
-    Time: '17:30'
+    cost: '85'
   },
    {
     id:6,
     image: './asset/shrimp.jpg',
     name:'Shrimp',
-    Time: '14:40'
+    Time: '100'
   },
    {
     id:7,
     image: './asset/pizza_2in_1.jpg',
-    name:'Pizza two in one'
+    name:'Pizza two in one',
+    cost:'80'
   },
    {
     id:8,
     image: './asset/pepperoni-i-tomatyi.jpg',
-    name:'Pepperoni i tomatyi'
+    name:'Pepperoni i tomatyi',
+    cost:'70'
   },
     {
     id:9,
     image: './asset/OIP_mix.jpg',
-    name:'OIP mix'
+    name:'OIP mix',
+    cost:'80'
   }
 ];
