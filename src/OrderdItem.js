a529aedbd0aa1cb576db13ce425583a3fbdc8468
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-12-16 09:37:02 +0100

    sum the cost in Order component

--- src/OrderdItem.js
+++ src/OrderdItem.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './OrderdItem.css'
 
 const OrderdItem = (props)=>{
+  const sum = (accumulator, currentOrderdItem) =>{ 
+    return accumulator + currentOrderdItem.cost
+   }
   return <div className='rightNav'>
     {
       props.orderdItems.map(
@@ -9,10 +12,12 @@ const OrderdItem = (props)=>{
           <label>Name: {item.name}</label><br/>
           <label>Cost: {item.cost}</label><br/>
           <button onClick={()=>props.removeOrderdItem(item)}>Remove</button>
-        </div>
-      
+        </div>              
       )
     }
+    <div>
+    Total: {props.orderdItems.reduce(sum,0)}    
+    </div>
   </div>
 }
 export default OrderdItem
--- src/data.js
+++ src/data.js
@@ -3,37 +3,37 @@ export default[
     id:1,
     image: './asset/bacon_tomato.png',
     name: 'Bacon tomato',
-    cost: '120'
+    cost: 120
   },
   {
     id:2,
     image: './asset/salami.jpg',
     name: 'Salami',
-    cost: '110'
+    cost: 110
   },
    {
     id:3,
     image: './asset/salami_bacon_paper.jpg',
     name: 'salami bacon paper',
-    cost: '99'
+    cost: 99
   },
    {
     id:4,
     image: './asset/vegetarian.png',
     name: 'Vegetarian',
-    cost: '70'
+    cost: 70
   },
    {
     id:5,
     image: './asset/OIP.jpg',
     name: 'OIP',
-    cost: '85'
+    cost: 85
   },
    {
     id:6,
     image: './asset/shrimp.jpg',
     name:'Shrimp',
-    Time: '100'
+    Time: 100
   },
    {
     id:7,
@@ -45,12 +45,12 @@ export default[
     id:8,
     image: './asset/pepperoni-i-tomatyi.jpg',
     name:'Pepperoni i tomatyi',
-    cost:'70'
+    cost:70
   },
     {
     id:9,
     image: './asset/OIP_mix.jpg',
     name:'OIP mix',
-    cost:'80'
+    cost:80
   }
 ];
