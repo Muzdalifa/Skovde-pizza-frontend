64859adda8a44e4dc3a8a50c48b18e41603ac018
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-12-17 11:21:46 +0100

    layouting App components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
-import React, {useState} from 'react';
-import data from './data';
-import Menu from './Menu';
+import React from 'react';
 import Header from './Header';
-import OrderdItem from './OrderdItem';
-import Details from './Details';
-import './App.css'
+import Main from './Main';
 import Footer from './Footer';
+import './App.css'
 
 
 const App = () => {
 
-  const [selectedItem, setSelectedItem] = useState({id: '',image:'',name:'', Time:''});
-  const [orderdItems,setOrderdItems] = useState([]);
-
-  const addOrderdItem=(item)=>{
-    orderdItems.push(item);
-    setOrderdItems([...orderdItems]);
-  }
-
-  const removeOrderdItem=(item)=>{
-    const itemIndex = orderdItems.findIndex((x)=>x.id===item.id)
-    orderdItems.splice(itemIndex,1)
-    setOrderdItems([...orderdItems])
-  }
-
-  const menuProps = { data, selectedItem, setSelectedItem }
-  const detailsProps = { selectedItem, addOrderdItem }
-  const orderdItemsProps = {orderdItems,setOrderdItems,removeOrderdItem}
-
   return (
-    <div className='container'>
-      < Header/>
-      <div className='main'>
-        <Menu { ...menuProps}   />
-        <Details {...detailsProps}  />
-        <OrderdItem {...orderdItemsProps}/>
-      </div>
-      <Footer/>
+    <div>
+      <Header class='header'/>
+      <Main class='main' />
+      <Footer class='footer'/>
     </div>
   )
   }
