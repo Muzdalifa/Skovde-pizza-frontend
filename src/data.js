ab5cb07d60cd9bc6f216e9d491c50918e673f835
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-12-24 04:06:35 +0100

    change id of pizza data

--- src/data.js
+++ src/data.js
@@ -1,54 +1,54 @@
 export default[
   {
-    id:1,
+    id:100,
     image: './asset/bacon_tomato.png',
     name: 'Bacon tomato',
     cost: 120,
   },
   {
-    id:2,
+    id:101,
     image: './asset/salami.jpg',
     name: 'Salami',
     cost: 110
   },
    {
-    id:3,
+    id:102,
     image: './asset/salami_bacon_paper.jpg',
     name: 'Salami bacon paper',
     cost: 99
   },
    {
-    id:4,
+    id:103,
     image: './asset/vegetarian.png',
     name: 'Vegetarian',
     cost: 70
   },
    {
-    id:5,
+    id:104,
     image: './asset/OIP.jpg',
     name: 'Mashoom',
     cost: 85
   },
    {
-    id:6,
+    id:105,
     image: './asset/shrimp.jpg',
     name:'Shrimp',
     Time: 100
   },
    {
-    id:7,
+    id:106,
     image: './asset/pizza_2in_1.jpg',
     name:'Pizza two in one',
     cost:'80'
   },
    {
-    id:8,
+    id:107,
     image: './asset/pepperoni-i-tomatyi.jpg',
     name:'Pepperoni i tomatyi',
     cost:70
   },
     {
-    id:9,
+    id:108,
     image: './asset/OIP_mix.jpg',
     name:'Mashroom and salami',
     cost:80
