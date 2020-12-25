9e1aba1a82e9d1ab8471fb7520cf3e7944899148
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-12-25 02:18:20 +0100

    Styling orderItem component

--- src/OrderdItem.js
+++ src/OrderdItem.js
@@ -7,27 +7,35 @@ const OrderdItem = (props)=>{
     
     return accumulator + currentOrderdItemInfo.item.cost * currentOrderdItemInfo.count
    } 
-  console.log(props.orderdItems);
+ 
   return <div className='rightNav'>
+    <h2>Köpta produkter</h2>
     {
       props.orderdItems.map(
-        (orderItemInfo,index)=><div key={index} className='rightNavUp' >
+        (orderItemInfo,index)=><div key={index} className='rightNavUp'>
           <label>{orderItemInfo.item.name}</label>
           <label>å{orderItemInfo.item.cost}</label>
-          <label onClick={()=>props.removeOrderdItem(orderItemInfo.item)}>-</label>
-          <label >{orderItemInfo.count}</label>
-          <label onClick={()=>props.addOrderdItem(orderItemInfo.item)}>+</label>
+          <div className='orderCount'>
+          <label className='labelbtn' onClick={()=>props.removeOrderdItem(orderItemInfo.item)}>-</label>
+          <label className='countItem'>{orderItemInfo.count}</label>
+          <label className='labelbtn' onClick={()=>props.addOrderdItem(orderItemInfo.item)}
+            onMouseDown={(event)=>console.log('mouseDown',event.target.classList)}
+            onMouseUp={(event)=>console.log('mouseUp',event.target)}
+          >+</label>
+          </div>
           <label>{orderItemInfo.item.cost*orderItemInfo.count}:-</label>
-          <button onClick={()=>props.removeOrderdItem(orderItemInfo.item, true)}>Remove</button>
+          <button className='orderItemRemoveBtn' onClick={()=>props.removeOrderdItem(orderItemInfo.item, true)}>X</button>
         </div>              
       )
-    }
-    <div>
-    Total: {props.orderdItems.reduce(sum,0)}    
-    </div>
-    <div className='rightNavDown'>
+    } 
+  <div className='rightNavTotalPayment'>
+    <label className='labelTotal'>Att betala:</label>
+    <label className='total'>{props.orderdItems.reduce(sum,0)} :-</label> 
+    <label>{''}</label>  
+  </div>
+  <div className='rightNavDown'>
 
-    </div>
+  </div>
   </div>
 }
 export default OrderdItem
