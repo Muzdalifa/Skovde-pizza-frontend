6e9854b130897b6ed755ef52c7fe1bb42568a6c7
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-12-27 06:22:29 +0100

    Styling header

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,11 @@ import './Header.css'
 const Header = ()=>{
   return <header>
     <h3>Skövde Pizza</h3>
+    <div className='nav'>
+      <a>Hem</a>
+      <a>Om Oss</a>
+      <a>Kontakt</a>
+    </div>
   </header>
 }
 export default Header
