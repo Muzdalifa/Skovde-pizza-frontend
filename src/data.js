1b8271fe038c714afc41a37e029103d1d5280d2e
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-12-31 10:06:05 +0100

    styling orderItem component

--- src/data.js
+++ src/data.js
@@ -4,24 +4,28 @@ export default[
     image: './asset/bacon_tomato.png',
     name: 'Bacon tomato',
     cost: 120,
+    ingredients: 'Bacon, ',
   },
   {
     id:101,
     image: './asset/salami.jpg',
     name: 'Salami',
-    cost: 110
+    cost: 110,
+    ingredients:'',
   },
    {
     id:102,
     image: './asset/salami_bacon_paper.jpg',
     name: 'Salami bacon paper',
-    cost: 99
+    cost: 99,
+    ingredients:'',
   },
    {
     id:103,
     image: './asset/vegetarian.png',
     name: 'Vegetarian',
-    cost: 70
+    cost: 70,
+    ingredients:'',
   },
    {
     id:104,
@@ -33,24 +37,28 @@ export default[
     id:105,
     image: './asset/shrimp.jpg',
     name:'Shrimp',
-    cost: 90
+    cost: 90,
+    ingredients:'',
   },
    {
     id:106,
     image: './asset/pizza_2in_1.jpg',
     name:'Pizza two in one',
-    cost:80
+    cost:80,
+    ingredients:'',
   },
    {
     id:107,
     image: './asset/pepperoni-i-tomatyi.jpg',
     name:'Pepperoni i tomatyi',
-    cost:70
+    cost:70,
+    ingredients:'',
   },
     {
     id:108,
     image: './asset/OIP_mix.jpg',
     name:'Mashroom and salami',
-    cost:80
+    cost:80,
+    ingredients:'',
   }
 ];
