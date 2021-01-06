f54b43ece3bc96865a5aa3f4e8d589004196f5e7
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2021-01-06 14:27:14 +0100

    styling Payment component

--- src/Payment.js
+++ src/Payment.js
@@ -9,45 +9,28 @@ const Payment = ()=>{
 
   return <div>
     <p>Order number {state.orderNum}</p>
-    <form className='form-container'>
-      <div>
-      <label>Förnamn: </label>
+    <div className='payment-info'>
+      <form className='form-container'>
+        <input type ='text' placeholder='Förnamn'></input>
+        <input type ='text' placeholder='Efternamn'></input>
+        <input type ='text' placeholder='Address'></input>
+      </form>
+      <div className='payment-item'>
+        <h4>Köpta produkter</h4>
+        {
+          state.orderItems.map(
+            (orderItemInfo,index)=><div key={index} className='order-item-right-nav-up'>
+              <label>{orderItemInfo.item.name}</label>
+              <label className='order-item-numericcell'>{orderItemInfo.item.cost}x{orderItemInfo.count}</label> 
+              <label className='order-item-numericcell'>{orderItemInfo.item.cost*orderItemInfo.count}:-</label><br/>
+            </div>              
+          )
+        }
+        <label className='order-item-numericcell'>Total betalning:{state.cost}:-</label>
       </div>
-      <div>
-      <input type ='text' placeholder='Förnamn'></input>
-      </div>
-      <div>
-      <label>Efternamn: </label>
-      </div>
-      <div>
-      <input type ='text' placeholder='Efternamn'></input><br />
-      </div>
-      <div>
-      <label>Adress: </label>
-      </div>
-      <div>
-      <input type ='text' placeholder='Address'></input><br />
-      </div>
-      <div>Produkt</div>
-      <input type='text'></input>
-    </form>
-
-    <h4>Köpta produkter</h4>
-    {
-      state.orderItems.map(
-        (orderItemInfo,index)=><div key={index} className='order-item-right-nav-up'>
-          <label>{orderItemInfo.item.name}</label>
-          <label className='order-item-numericcell'>å{orderItemInfo.item.cost}</label>
-          <div className='order-item-order-count'>
-          <label className='order-item-count-item'>{orderItemInfo.count}</label>
-          </div>
-          <label className='order-item-numericcell'>{orderItemInfo.item.cost*orderItemInfo.count}:-</label>
-        </div>              
-      )
-    }
+    </div>
 
     <div className='payment-final-payments'> 
-        <p>Total betalning: {state.cost}</p>
         <p>Betalningar</p>
     </div>
   </div>
