796d9e0ee63f1db637d732839beb8f189f04893d
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2021-01-15 19:40:48 +0100

    chekpoint: responsive design (ipad)

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import './Home.css'
+import {useHistory} from 'react-router-dom';
+
 const Home = () =>{
+  
+  const history = useHistory();
+
   return <div className='home-main-content'>
-    <img className='home-image' src='./asset/logo/home_page_img.jpg' ></img>
     <div className='home-left-div'>
+      <div className='home-div-before-image'>
+        <img className='home-image' src='./asset/logo/home_page_img.jpg' ></img>
+      </div>
+    </div>
+    <div className='home-right-div'>
       <h1>Välkommen</h1>
       <div className='home-info'>      
         Skövde pizza är en familjeägt och drivs restaurang belägen i Skövde Centrum.
         Vi är speciapserade på djuprätt och traditionell skordiva på västkusten,
         serverad med en rad färska sallader, aptitretare och smörgåsar - allt 
         från grunden varje dag.<br />
-        Njut av vår varma och välkomnande atmosfär på vår expansiva uteplats eller i vår vackra fullservice matsal.
+        <b><i>Njut av vår varma och välkomnande atmosfär på vår expansiva uteplats eller i vår vackra fullservice matsal.</i></b>
       </div>
       <div>
         <h2>Öppettider</h2>
@@ -22,13 +31,17 @@ const Home = () =>{
             <div>Sön </div>
           </div>
           <div className='home-open-hour'>
-            <div>11.00-23.00</div>
-            <div>11.00-22.00</div>
-            <div>12.00-23.00</div>          
-            <div>12.00-22.00</div>
+            <div>11.00&ensp;-&ensp;23.00</div>
+            <div>11.00&ensp;-&ensp;22.00</div>
+            <div>12.00&ensp;-&ensp;23.00</div>          
+            <div>12.00&ensp;-&ensp;22.00</div>
         </div>
-      </div> 
+      </div>
     </div>
+    <div className='home-button'>
+      <button className='home-orderbtn' onClick={()=> history.push('/order')}>Beställa</button>
+    </div>
+    
     </div>
   </div>
 }
