c959e0dc21428ac684c6d83bc717f0381288e053
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2021-01-20 20:08:08 +0100

    Add order page

---
+++ src/components/OrderPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const OrderPage = () =>{
+  return <div className="row">
+    <div className="side">
+      <h2></h2>
+      <h5></h5>
+      <div className="fakeimg" style={{height:200}}>Image</div>
+    </div>
+    <div className="side">
+      <h2></h2>
+      <h5></h5>
+      <div className="fakeimg" style={{height:200}}>Image</div>
+    </div>
+    <div className="side">
+      <h2></h2>
+      <h5></h5>
+      <div className="fakeimg" style={{height:200}}>Image</div>
+    </div>
+
+  </div>
+}
+
+export default OrderPage;
