17e3472f3f92f3d0b425374c5701af6ef1b8ac62
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2021-01-20 20:06:53 +0100

    Add contact page

---
+++ src/components/ContactPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContactPage = () =>{
+  return <div className="row">
+  <div className="side">
+    <h2></h2>
+    <h5></h5>
+    <div className="fakeimg" style={{height:200}}>Image</div>
+  </div>
+  </div>
+}
+
+export default ContactPage;
