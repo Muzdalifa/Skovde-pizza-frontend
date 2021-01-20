95a5059657fffa9bcceb132417e2f1e0d0b1bdb6
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2021-01-20 20:07:46 +0100

    Add link to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Navigation.css"
+
+import {Link, useLocation} from "react-router-dom"
+
 const Navigation = () =>{
+  const {pathname} = useLocation()
   return <div className="navbar">
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
+          <Link className= {`header-link ${pathname==='/'? 'header-link-active':'' }`} to='/' >Hem </Link>
+          <Link className={`header-link ${pathname==='/order'? 'header-link-active':'' }`} to='/order' >Beställa</Link>
+          <Link className= {`header-link ${pathname==='/kontakt'? 'header-link-active':'' }`} to='/kontakt'  >Kontakt</Link>  
       </div>
 }
 export default Navigation;
